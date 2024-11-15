672c85625b32f52d4a221f0836aae6e038be3f61
olgieryan <123715377+olgieryan@users.noreply.github.com>
2024-11-15 13:21:36 +0800

    commit

--- main.tf
+++ main.tf
@@ -48,11 +48,30 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "terra-alb"
+
+  load_balancer_type = "application"
+
   vpc_id  = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
 
 http_tcp_listeners = [
   {
@@ -61,16 +80,6 @@ http_tcp_listeners = [
     target_group_index = 0
   }
 ]
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
 
   tags = {
     Environment = "Development"
