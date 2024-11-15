d89c38c4bd50bf5aa46016f8459d5680c41331d1
olgieryan <123715377+olgieryan@users.noreply.github.com>
2024-11-15 10:22:17 +0800

    create ALB

--- main.tf
+++ main.tf
@@ -46,6 +46,36 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "terra-alb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+  security_group = module.blog_sg.security_group_id
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
