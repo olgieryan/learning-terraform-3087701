d31630756531cdeabfa9ba61434cd665b571b9fd
olgieryan <123715377+olgieryan@users.noreply.github.com>
2024-11-15 14:02:15 +0800

    commit

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.vpc.public_subnets
-  target_group_arns = module.alb.target_group_arns
+  target_group_arn = module.alb.target_group_arn
   security_groups = [module.blog_sg.security_group_id]
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
