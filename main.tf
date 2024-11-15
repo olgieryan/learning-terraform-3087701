2266e77f4b52ac46bfcfbaf0ff4c455c6ab86788
olgieryan <123715377+olgieryan@users.noreply.github.com>
2024-11-15 10:42:45 +0800

    commit

--- main.tf
+++ main.tf
@@ -58,8 +58,20 @@ listeners = {
     ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
-      target_group_index = 0
-      
+  redirect = {
+    port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
     }
   }
 
