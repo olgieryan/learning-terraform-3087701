0c65129b671fe9fa8caa894c8d7ddb8b0def4f5e
olgieryan <123715377+olgieryan@users.noreply.github.com>
2024-11-15 09:40:52 +0800

    create security group module

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group_id]
+  vpc_security_group_ids = [module.blog_sg.aws_security_group_id]
 
   tags = {
     Name = "Terra_Test"
